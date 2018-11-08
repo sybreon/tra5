407f0747186007ce3ee60a42e61150ebfe8d1d22
Shawn Tan <shawn.tan@aeste.net>
2018-11-08 13:53:49 +0800

    Added license statement + hart switcher.

--- rtl/verilog/t5_inst.v
+++ rtl/verilog/t5_inst.v
@@ -1,3 +1,19 @@
+/*
+Copyright 2018 Aeste Works (M) Sdn Bhd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 module t5_inst(/*AUTOARG*/
    // Outputs
    pc, iadr,
@@ -20,10 +36,19 @@ module t5_inst(/*AUTOARG*/
 		     rst;
         
    reg [XLEN-1:2]    iadr;
-   reg [1:0] 	     hart;
-   
+   reg [1:0] 	     hart;   
    reg [XLEN-1:0]    pc;   
 
+   // HART SWITCHER
+   always @(posedge clk)
+     if (rst)
+       /*AUTORESET*/
+       // Beginning of autoreset for uninitialized flops
+       hart <= 2'h0;
+       // End of automatics
+     else if (ena)
+       hart <= {hart[0],~hart[1]};   
+   
    // PC REGISTER
    always @(posedge clk)
      if (rst)
@@ -45,7 +70,6 @@ module t5_inst(/*AUTOARG*/
        case (bra)
 	 1'b1: iadr <= alu;
 	 default: iadr <= npc;	   
-       endcase // case (bra)
-   
+       endcase // case (bra)     
    
 endmodule // t5_inst
